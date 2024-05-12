999d81a834d7e26f37ebe3aee9d6693e7b4799bc
BlackPalaces <146561620+BlackPalaces@users.noreply.github.com>
2024-05-12 16:11:52 +0700

    Initial Commit

---
+++ AntiquesApp/AntiquesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AntiquesAppApp.swift
+//  AntiquesApp
+//
+//  Created by Chinnaphat Dumpong on 12/5/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct AntiquesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
